23ae9b3b1d7ba90cc294fe44ec23a0a53dd8932f
Luis Felipe Rosas <contact@luisfeliperosas.com>
2013-12-10 10:49:05 -0500

    Base HMTL

---
+++ html/frontend/js/main.js
@@ -0,0 +1,18 @@
+/* ==========================================================================
+*   TP2 - Mise en page Internet II.
+*	Utilisation d'un front-end framework comme HTML5 Boilerplate et de Bootstrap
+*	Prof.	: Jonathan Martel
+*	Author	: Luis Felipe Rosas
+*	Date	: 28-11-2013
+   ========================================================================== */
+
+// Function pour faire fonctioner le POPOVER de BOOTSTRAP
+!function ($) {
+
+	$(function(){
+  
+		$("[data-toggle=popover]").popover();
+
+	})
+
+}(window.jQuery)
