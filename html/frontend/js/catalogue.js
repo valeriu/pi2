79e4a8e2c945cc87609b5c9ce648fb87adc07564
Yan Boucher Bouchard <yan.bbouchard@gmail.com>
2013-12-11 15:30:59 -0500

    Module Catalogue v.0.7
    - Ajout des zones pour les suggestions et les derniers commentaires
    - minor bug fixes

--- html/frontend/js/catalogue.js
+++ html/frontend/js/catalogue.js
@@ -5,21 +5,15 @@ $(function() {
     var large={width: "+=60px",height: "+=60px"};
     var small={width: "-=60px",height: "-=60px"};
 
-    $("td[colspan=7]").find("div.product-details").hide();
-
     $("td:not([colspan=7])").click(function(event) {
         event.stopPropagation();
         var $target = $(event.target);
 
         //TOGGLE DETAILS IN CATALOG
-        if ( $target.closest("td").attr("colspan") > 1 ) {
-            $target.slideUp();
-        } else {
             //TOGGLE IMAGE SIZE IN CATALOG
             (count==1)?$target.parent().find("img").animate(large, "slow", function() {
                 $target.closest("tr").next().find("div.product-details").slideToggle("slow");}):$target.closest("tr").next().find("div.product-details").slideToggle("slow", function() {
                     $target.parent().find("img").animate(small, "slow")});
-            count = 1-count; 
-        }                   
+            count = 1-count;                   
     });
 });
