f4eadca4587323dd22540ae457de60b2f54ae2ac
Yan Boucher Bouchard <yan.bbouchard@gmail.com>
2013-12-12 09:54:10 -0500

    Module Catalogue v.0.8.4
    - Collapse fixed!

--- html/frontend/js/catalogue.js
+++ html/frontend/js/catalogue.js
@@ -18,17 +18,27 @@ $(function() {
                     count = 2;
                 });
             } else {
-                $("article.visible-details").slideToggle("slow", function() {
-                    $("article.visible-details").toggleClass("visible-details");
-                    $("img.large-image").animate(small, "slow", function() {
-                        $("img.large-image").toggleClass("large-image");
-                        $target.parent().find("img").animate(large, "slow", function() {
-                            $target.closest("tr").next().find("article.product-details").slideToggle("slow");
-                            $target.closest("tr").next().find("article.product-details").toggleClass("visible-details");
-                            $target.parent().find("img").toggleClass("large-image");
+                if($target.closest("tr").next().find("article.product-details").hasClass("visible-details")) {
+                    $("article.visible-details").slideToggle("slow", function() {
+                        $("article.visible-details").toggleClass("visible-details");
+                        $("img.large-image").animate(small, "slow", function() {
+                            $("img.large-image").toggleClass("large-image");
+                            count = 1;
                         });
-                    });      
-                });       
+                    });
+                } else {
+                    $("article.visible-details").slideToggle("slow", function() {
+                        $("article.visible-details").toggleClass("visible-details");
+                        $("img.large-image").animate(small, "slow", function() {
+                            $("img.large-image").toggleClass("large-image");
+                            $target.parent().find("img").animate(large, "slow", function() {
+                                $target.closest("tr").next().find("article.product-details").slideToggle("slow");
+                                $target.closest("tr").next().find("article.product-details").toggleClass("visible-details");
+                                $target.parent().find("img").toggleClass("large-image");
+                            });
+                        });      
+                    });
+                }           
             }                
     });
 });
