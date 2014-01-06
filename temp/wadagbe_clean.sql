25e613544d160440cfbc4f50f26092cda6bac9ad
Valeriu Tihai <valeriu@tihai.md>
2014-01-06 12:37:47 -0500

    sql
    *.sql vide et avec l'informations

--- temp/wadagbe_clean.sql
+++ temp/wadagbe_clean.sql
@@ -2,19 +2,19 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `wadagbe` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `wadagbe` ;
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`pages`
+-- Table `e1295805`.`wa_pages`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`pages` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_pages` (
   `id_page` INT NOT NULL AUTO_INCREMENT ,
   `titre` VARCHAR(250) NOT NULL ,
   `description_meta` VARCHAR(250) NULL ,
   `contenu` LONGTEXT NULL ,
   `date` DATETIME NOT NULL ,
   `statut` INT NOT NULL DEFAULT 0 COMMENT '0 - invisible\n1 - visible\n2 - supprimé' ,
+  `long` FLOAT NULL ,
+  `lat` FLOAT NULL ,
   PRIMARY KEY (`id_page`) )
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
@@ -22,28 +22,26 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`utilisateurs`
+-- Table `e1295805`.`wa_utilisateurs`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`utilisateurs` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_utilisateurs` (
   `id_utilisateurs` INT NOT NULL ,
   `courriel` VARCHAR(200) NOT NULL ,
-  `nom_utilisateur` VARCHAR(15) NOT NULL ,
   `mot_passe` VARCHAR(64) NOT NULL ,
   `nom_prenom` VARCHAR(250) NOT NULL ,
   `date` DATETIME NOT NULL ,
   `role` INT NOT NULL DEFAULT 0 COMMENT '0 - users\n1 - administator\n2 - super administator' ,
-  `cle_activation` VARCHAR(100) NOT NULL ,
+  `cle_reactivation` INT NOT NULL ,
   `statut` INT NULL DEFAULT 1 COMMENT '1 - actif\n2 - supprimé' ,
   PRIMARY KEY (`id_utilisateurs`) ,
-  UNIQUE INDEX `mail_UNIQUE` (`courriel` ASC) ,
-  UNIQUE INDEX `username_UNIQUE` (`nom_utilisateur` ASC) )
+  UNIQUE INDEX `mail_UNIQUE` (`courriel` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`evaluation`
+-- Table `e1295805`.`wa_evaluation`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`evaluation` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_evaluation` (
   `id_evaluation` INT NOT NULL AUTO_INCREMENT ,
   `votes` INT NULL DEFAULT 0 ,
   `evaluation_total` INT NULL DEFAULT 0 ,
@@ -54,9 +52,9 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`categorie`
+-- Table `e1295805`.`wa_categorie`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`categorie` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_categorie` (
   `id_categorie` INT NOT NULL AUTO_INCREMENT ,
   `nom` VARCHAR(250) NOT NULL ,
   PRIMARY KEY (`id_categorie`) )
@@ -66,9 +64,9 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`produits`
+-- Table `e1295805`.`wa_produits`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`produits` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_produits` (
   `id_produits` INT NOT NULL ,
   `nom` VARCHAR(250) NULL ,
   `prix` INT NULL ,
@@ -85,6 +83,10 @@ CREATE  TABLE IF NOT EXISTS `wadagbe`.`produits` (
   `taille_hauteur` FLOAT NULL ,
   `evaluation_id_evaluation` INT NOT NULL ,
   `categorie_id_categorie` INT NOT NULL ,
+  `puissance` INT NULL ,
+  `voc` FLOAT NULL COMMENT 'voc -  la tension qui est présente lorsqu’il ne circule aucun courant' ,
+  `vmp` FLOAT NULL COMMENT 'La tension Vmp (pour tension à puissance maximale)' ,
+  `produitscol` VARCHAR(45) NULL ,
   PRIMARY KEY (`id_produits`) ,
   INDEX `fk_produits_evaluation1_idx` (`evaluation_id_evaluation` ASC) ,
   INDEX `fk_produits_categorie1_idx` (`categorie_id_categorie` ASC) )
@@ -94,9 +96,9 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`adresse`
+-- Table `e1295805`.`wa_adresse`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`adresse` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_adresse` (
   `id_adresse` INT NOT NULL AUTO_INCREMENT ,
   `telephone` INT NOT NULL ,
   `rue` VARCHAR(45) NOT NULL ,
@@ -111,9 +113,9 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`adresse_utilisateur`
+-- Table `e1295805`.`wa_adresse_utilisateur`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`adresse_utilisateur` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_adresse_utilisateur` (
   `id_adresse_utilisateur` INT NOT NULL ,
   `adresse_id_adresse` INT NOT NULL ,
   `utilisateurs_id_utilisateurs` INT NOT NULL ,
@@ -126,13 +128,14 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`commandes`
+-- Table `e1295805`.`wa_commandes`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`commandes` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_commandes` (
   `id_commandes` INT NOT NULL AUTO_INCREMENT ,
   `statut` INT NOT NULL DEFAULT 0 COMMENT '0 - en traitement\n1 - backorder\n2 - expedie' ,
   `commentaires` VARCHAR(250) NULL ,
   `adresse_utilisateur_id_adresse_utilisateur` INT NOT NULL ,
+  `date` DATETIME NULL ,
   PRIMARY KEY (`id_commandes`) ,
   INDEX `fk_ordes_adresse_utilisateur1_idx` (`adresse_utilisateur_id_adresse_utilisateur` ASC) )
 ENGINE = MyISAM
@@ -141,9 +144,9 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`commande_produit`
+-- Table `e1295805`.`wa_commande_produit`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`commande_produit` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_commande_produit` (
   `id_commande_produit` INT NOT NULL AUTO_INCREMENT ,
   `quantite` INT NOT NULL ,
   `produits_id_produits` INT NOT NULL ,
@@ -157,9 +160,9 @@ COLLATE = utf8_general_ci;
 
 
 -- -----------------------------------------------------
--- Table `wadagbe`.`menu`
+-- Table `e1295805`.`wa_menu`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `wadagbe`.`menu` (
+CREATE  TABLE IF NOT EXISTS `e1295805`.`wa_menu` (
   `id_menu` INT NOT NULL AUTO_INCREMENT ,
   `titre` VARCHAR(45) NOT NULL ,
   `description` VARCHAR(45) NULL ,
@@ -172,7 +175,6 @@ ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
-USE `wadagbe` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
