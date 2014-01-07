6c65cad996d695bb8d1bccd6c0babe1ff05d8db7
Valeriu Tihai <valeriu@tihai.md>
2014-01-07 17:20:38 -0500

    Test Module Pages :angry:
    reconstruite :angry:

--- test/gabarit.pages.test.php
+++ test/gabarit.pages.test.php
@@ -1,41 +1,52 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-
 	<head>
-
-		<title>Test unitaire</title>
+		<title>Test Pages</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	</head>
 
 	<body>
 		<div id="header">
-			<h1>Test - Modèles - Usagers</h1>
+			<h1>Test - Modèles - Pages</h1>
 		</div>
 		<div id="contenu">
-			<h1>Test: connecter() fonctionnel</h1>
+			<h1>Test: ajouter ($titre, $description_meta, $contenu, $statut, $geo_long, $geo_lat) fonctionnel</h1>
 			<?php 
-				$usagers = new Usagers();
-				$rep = $usagers->connecter("per.inceptos@blandit.com", "pass");
-				var_dump($rep);
-			?>
-			<h1>Test: connecter() non-fonctionnel</h1>
-			<?php 
-				$usagers = new Usagers();
-				$rep = $usagers->connecter("test", "pass");
-				var_dump($rep);
-			?>
-		</div>
-		<div id="footer">
+				$page = new Pages();
+				$result = $page->ajouter ("titre", "description_meta", "contenu", 1, 22.55, 55.888);
 
+				var_dump($result);
+			?>
+
+			<h1>Test: modifier ($id_page, $titre, $description_meta, $contenu, $date, $statut, $geo_long, $geo_lat) fonctionnel</h1>
+			<?php 
+				$page = new Pages();
+				$date = date("Y-m-d H:i:s");
+				$result = $page->modifier (110, "titre", "description_meta", "contenu", $date, 1, 22.55, 55.888);
+
+				var_dump($result);
+			?>
+		<h1>Test: afficher ($id_page) fonctionnel</h1>
+		<pre>
+			<?php 
+				$page = new Pages();
+				$result = $page->afficher (99);
+
+				var_dump($result);
+			?>	
+		</pre>
+		
+		
+		<h1>Test: afficherListe() - fonctionnel</h1>
+		<pre>
+			<?php 
+				$page = new Pages();
+				$result = $page->afficherListe();
+
+				var_dump($result);
+			?>	
+		</pre>		
+		
 		</div>
 	</body>
 </html>
-
-
-
-
-
-
-
-
