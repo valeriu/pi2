ad36245298875366878959d9bff28892b9dd687a
Valeriu Tihai <valeriu@tihai.md>
2014-01-08 12:31:33 -0500

    Modele Valider
    Ajouter methode AlphaNumerique  String et modifie autres methodes

--- modeles/Valider.class.php
+++ modeles/Valider.class.php
@@ -15,7 +15,7 @@ abstract class Valider {
 	 * @return boolean 
 	 */
 	public function estInt($valeur) {
-		return TRUE;
+		is_int($valeur);
 	}
 	
 	/**
@@ -26,7 +26,29 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estFloat($valeur) {
-		return TRUE;
+		is_float($valeur);
+	}
+	
+	/**
+	 * Vérifiez si la $valeur est AlphaNumerique
+	 * 
+	 * @param type $valeur
+	 * 
+	 * @return boolean
+	 */
+	public function estAlphaNumerique($valeur){
+		ctype_alnum($valeur);
+	}
+	
+	/**
+	 * Vérifiez si la $valeur est String
+	 * 
+	 * @param type $valeur
+	 * 
+	 * @return boolean
+	 */
+	public function estString($valeur){
+		 is_string($valeur);
 	}
 	
 	/**
@@ -37,7 +59,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estNegatif($valeur){
-		return TRUE;
+		$valeurNegatif = ($valeur < 0 ? true : false);
 	}
 	
 	/**
@@ -50,7 +72,8 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estEntre($valeur, $nb1, $nb2){
-		return TRUE;
+		$valeur = strlen($valeur);
+		$valeurEntre = ((($nb1 <= $valeur) && ($valeur <= $nb2)) ? true : false);
 	}
 
 	/**
@@ -60,16 +83,21 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estCourriel ($valeur) {
-		return TRUE;
+		filter_var($valeur, FILTER_VALIDATE_EMAIL);
 	}
 	
 	/**
+	 * Vérifiez si $valeur est un date en fonction du format
 	 * 
+	 * URL source -> http://ca1.php.net/checkdate#113205
 	 * @param type $valeur
+	 * @param type $format
+	 * 
 	 * @return boolean
 	 */
-	public function estDate($valeur) {
-		return TRUE;
+	public function estDate($valeur, $format = 'Y-m-d H:i:s') {
+		$d = DateTime::createFromFormat($format, $valeur);
+		return $d && $d->format($format) == $valeur;
 	}
 	
 	/**
@@ -78,7 +106,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estURL($valeur) {
-		return TRUE;
+		filter_var($valeur, FILTER_VALIDATE_URL);
 	}
 	
 	/**
@@ -87,7 +115,19 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estImage($valeur) {
-		return TRUE;
+		$ext = strtolower(end(explode('.', $valeur)));
+			switch($ext) {
+				case "jpg": 
+				case "jpeg": 
+				case "jpe": 
+				case "png": 
+				case "gif": 
+				case "bmp":
+					return TRUE;
+					break;
+				default :
+					return FALSE;
+			}
 	}
 	
 	/**
