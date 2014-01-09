a7cda21c5ff79ab3c58228f34fed16302168814e
Valeriu Tihai <valeriu@tihai.md>
2014-01-09 11:26:46 -0500

    estURL,  estStringValide, estURL
    Valider::estVide($valeur);

--- modeles/Valider.class.php
+++ modeles/Valider.class.php
@@ -7,6 +7,12 @@
  */
 abstract class Valider {
 	
+	public function estVide($valeur=null){
+		if(empty($valeur)){
+			throw new Exception("Champ est vide");
+		}
+	}
+	
 	/**
 	 * Vérifiez si la chaîne $valeur est un nombre
 	 * 
@@ -15,6 +21,7 @@ abstract class Valider {
 	 * @return boolean 
 	 */
 	public function estInt($valeur) {
+		Valider::estVide($valeur);
 		return is_int($valeur);
 	}
 	
@@ -26,6 +33,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estFloat($valeur) {
+		Valider::estVide($valeur);
 		return is_float($valeur);
 	}
 	
@@ -37,6 +45,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estAlphaNumerique($valeur){
+		Valider::estVide($valeur);
 		return ctype_alnum($valeur);
 	}
 	
@@ -48,7 +57,8 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estString($valeur){
-		 return is_string($valeur);
+		Valider::estVide($valeur);
+		return is_string($valeur);
 	}
 	
 	/**
@@ -59,6 +69,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estNegatif($valeur){
+		Valider::estVide($valeur);
 		if ($valeur < 0)
 			return TRUE;
 		return FALSE;
@@ -70,6 +81,7 @@ abstract class Valider {
 	 * @return type
 	 */
 	public function estTableau($valeur){
+		Valider::estVide($valeur);
 		return is_array($valeur);
 	}
 
@@ -83,6 +95,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estEntreString($valeur, $nb1, $nb2){
+		Valider::estVide($valeur);
 		$valeurLength = strlen($valeur);
 		if (($nb1 <= $valeurLength) && ($valeurLength <= $nb2))
 			return TRUE;
@@ -100,6 +113,7 @@ abstract class Valider {
 	 *  @return boolean
 	 */
 	public function estEntreInt($valeur, $nb1, $nb2){
+		Valider::estVide($valeur);
 		if(!($nb1 >= $valeur) && ($valeur <= $nb2))
 			return TRUE;
 		return FALSE;
@@ -112,6 +126,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estCourriel ($valeur) {
+		Valider::estVide($valeur);
 		return filter_var($valeur, FILTER_VALIDATE_EMAIL);
 	}
 	
@@ -125,6 +140,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estDate($valeur, $format = 'Y-m-d H:i:s') {
+		Valider::estVide($valeur);
 		$d = DateTime::createFromFormat($format, $valeur);
 		return $d && $d->format($format) == $valeur;
 	}
@@ -137,7 +153,9 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estURL($valeur) {
-		filter_var($valeur, FILTER_VALIDATE_URL);
+		Valider::estVide($valeur);
+		$pattern = "/^https?:\/\/[a-z0-9-]+(\.[a-z0-9-]+)+/i";
+		return preg_match($pattern, $valeur);
 	}
 	
 	/**
@@ -148,6 +166,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estImage($valeur) {
+		Valider::estVide($valeur);
 		$ext = strtolower(end(explode('.', $valeur)));
 			switch($ext) {
 				case "jpg": 
@@ -172,7 +191,20 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estTel($valeur) {
+		Valider::estVide($valeur);
 		$pattern = "/^(800|844|855|866|877|888|900|403|587|780|250|604|778|236|204|431|506|709|902|782|226|249|289|343|416|519|613|647|705|807|905|418|438|450|514|579|581|819|873|306|639|867)\d{7}$/";
+		return preg_match($pattern, $valeur);
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param type $valeur
+	 * @return boolean
+	 */
+	public function estStringValide($valeur) {
+		Valider::estVide($valeur);
+		$pattern = "/^[a-zäâàèéêëïîôöüûùç0-9]+([ ,\.'-]?[a-z \.äâàèéêëïîôöüûùç)(0-9]+){1,}$/i";
 		return preg_match($pattern, $valeur);
 	}
 	
@@ -181,34 +213,8 @@ abstract class Valider {
 	 * @param type $valeur
 	 * @return boolean
 	 */
-	public function estRue($valeur) {
-		return TRUE;
-	}
-	
-	/**
-	 * 
-	 * @param type $valeur
-	 * @return boolean
-	 */
-	public function estApp($valeur) {
-		return TRUE;
-	}
-	
-	/**
-	 * 
-	 * @param type $valeur
-	 * @return boolean
-	 */
-	public function estVille($valeur) {
-		return TRUE;
-	}
-	
-	/**
-	 * 
-	 * @param type $valeur
-	 * @return boolean
-	 */
 	public function estCodePostal($valeur){
+		Valider::estVide($valeur);
 		$pattern = "/^[abceghjklmnprstvxy][0-9][abceghjklmnprstvwxyz][ ]?[0-9][[abceghjklmnprstvwxyz][0-9]$/i";
 		return preg_match($pattern, $valeur);
 	}
@@ -236,6 +242,7 @@ abstract class Valider {
 	 * @return boolean
 	 */
 	public function estProvince($valeur){
+		Valider::estVide($valeur);
 		$abb = strtolower($valeur);
 			switch($abb) {
 				case "ab": 
@@ -258,5 +265,4 @@ abstract class Valider {
 			}
 	}
 }
-
 ?>
