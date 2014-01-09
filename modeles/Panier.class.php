527ebc3e6d24392c7ce4a90a0ae0ed6d3f8bd2c0
Luis Felipe Rosas <contact@luisfeliperosas.com>
2014-01-09 08:31:27 -0500

    Modele PANIER v.0.3
    tous les tests c'est sont fait enregistrer()

--- modeles/Panier.class.php
+++ modeles/Panier.class.php
@@ -8,10 +8,107 @@
 /**
  * Description of Panier
  *
- * @author valeriu
+ * @author Luis
  */
 class Panier {
-	//put your code here
+	private $bd;
+	
+	public function __construct(){
+		$this->bd = BD::getInstance();
+	}
+
+	public function enregistrer ($aDonnees= array()) {
+		
+		(isset($aDonnees['id_utilisateur'])) ? $id_utilisateur = $aDonnees['id_utilisateur'] : $id_utilisateur = '';
+		(isset($aDonnees['adresse_utilisateur'])) ? $id_adresse = $aDonnees['adresse_utilisateur'] : $id_adresse = '';
+		(isset($aDonnees['quantite'])) ? $quantite = $aDonnees['quantite'] : $quantite = '';
+		(isset($aDonnees['produits'])) ? $produits = $aDonnees['produits'] : $produits = '';
+
+		if(!Valider::estInt($id_utilisateur))
+			throw new Exception("Un id d'utilisateur doit exister comme nombre!.");
+		if(!Valider::estInt($id_adresse))
+			throw new Exception("Un id d'addresse doit exister comme nombre!.");
+		if(!Valider::estInt($quantite))
+			throw new Exception("Une quantite doit être un entier positif!.");
+		if(!Valider::estTableau($produits))
+			throw new Exception("Les produits doivent être dans un tableau!.");
+
+		$idbd = $this->bd->getBD();
+		
+		// Permière étape ******************
+		$reqAdd = $idbd->prepare('SELECT id_adresse_utilisateur FROM wa_adresse_utilisateur WHERE adresse_id_adresse = ? AND utilisateurs_id_utilisateurs = ?');
+		$reqAdd->bindParam(1, $id_adresse);
+		$reqAdd->bindParam(2, $id_utilisateur);
+		$reqAdd->execute();
+
+		$verifAdd = $reqAdd->fetchAll();
+		
+		if(count($verifAdd) == 0)
+			throw new Exception("L'adresse n'est pas valide!.");
+
+		// Deuxième étape ******************
+		$reqPC = 'SELECT id_produits FROM wa_produits';
+		$resultat = $idbd->query($reqPC);
+
+		$produitsCatalogue = $resultat->fetchAll();
+
+		for($i = 0 ; $i < count($produits); $i++){
+			if(!Valider::estInt($produits[$i]['id_produits']))
+				throw new Exception("Les produits doivent être des entiers positifs !.");
+			if(!Valider::estInt($produits[$i]['quantite']))
+				throw new Exception("Le quantite de chacun des produits doivent être des entiers positifs !.");
+
+			$verfIdProduit[] = array('id_produits'=>$produits[$i]['id_produits']);
+		}
+
+		// Vérification de l'existance des produits
+		$comparaison = array_intersect($verfIdProduit, $produitsCatalogue);
+		if(count($comparaison) != count($verfIdProduit))
+			throw new Exception("Error de comparaison des tableaux!");
+			
+
+		// Troisième étape ******************
+		$dateCommande = date("Y-m-d H:i:s");
+
+		//BD wp_commandes
+		$reqC = $idbd->prepare(	"INSERT INTO wa_commandes
+								(id_commandes, statut, adresse_utilisateur_id_adresse_utilisateur, date_commande)
+								VALUES (null, 0, ?, ?)");
+
+		$reqC->bindParam(1, $verifAdd[0]['id_adresse_utilisateur']);
+		$reqC->bindParam(2, $dateCommande);
+		$resultat = $reqC->execute();
+
+		$idCommande = $idbd->lastInsertId();
+
+		if(!$resultat)
+			throw new Exception("Error d'insertion de la commande, table wa_commandes!");
+
+		// Quatrième étape ******************
+
+		//BD wp_commande_produit
+		$reqProduit = $idbd->prepare("INSERT INTO wa_commande_produit 
+									(quantite, produits_id_produits, commandes_id_commandes) 
+									VALUES (?, ?, ?)");
+
+		for($i = 0 ; $i < count($produits); $i++){
+			$reqProduit->bindParam(1, $produits[$i]['quantite']);
+			$reqProduit->bindParam(2, $produits[$i]['id_produits']);
+			$reqProduit->bindParam(3, $idCommande);
+			$resultat = $reqProduit->execute();	
+			
+			if(!$resultat)
+				throw new Exception("Error d'insertion de la commande par produit, table wa_commmade_produit!");
+		}
+		$idCommandeProduit = $idbd->lastInsertId();
+
+		if($idCommande && $idCommandeProduit)
+			return true;
+		else
+			return false;
+
+	}
+
 }
 
 ?>
--- test/gabarit.panier.test.php
+++ test/gabarit.panier.test.php
@@ -10,20 +10,118 @@
 
 	<body>
 		<div id="header">
-			<h1>Test - Modèles - Usagers</h1>
+			<h1>Test - Modèles - Panier</h1>
 		</div>
 		<div id="contenu">
-			<h1>Test: connecter() fonctionnel</h1>
-			<?php 
-				$usagers = new Usagers();
-				$rep = $usagers->connecter("per.inceptos@blandit.com", "pass");
-				var_dump($rep);
+			<h2>1) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => "98", "adresse_utilisateur" => 21, "quantite" => 2 , "produits" => array( array("id_produits" => 8 , "quantite" => 2), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
 			?>
-			<h1>Test: connecter() non-fonctionnel</h1>
-			<?php 
-				$usagers = new Usagers();
-				$rep = $usagers->connecter("test", "pass");
-				var_dump($rep);
+			<h2>2) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => "21", "quantite" => 2 , "produits" => array( array("id_produits" => 8 , "quantite" => 2), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h2>3) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 21, "quantite" => "2" , "produits" => array( array("id_produits" => 8 , "quantite" => 2), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h2>4) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 21, "quantite" => 2 , "produits" => 2);
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h2>5) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 21, "quantite" => 2 , "produits" => "2");
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h2>6) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 21, "quantite" => 2 , "produits" => array( array("id_produits" => "8" , "quantite" => 2), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h2>7) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 21, "quantite" => 2 , "produits" => array( array("id_produits" => 8 , "quantite" => "2"), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h2>8) Test: enregistrer() non-fonctionnel</h2>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "quantite" => 2 , "produits" => array( array("id_produits" => 8 , "quantite" => 2), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h1>9) Test: enregistrer() non-fonctionnel</h1>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 150, "quantite" => 2 , "produits" => array( array("id_produits" => 8 , "quantite" => 2), array("id_produits" => 68 , "quantite" => 6)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
+			?>
+			<h1>11) Test: enregistrer() fonctionnel</h1>
+			<?php
+				try{
+					$array = array("id_utilisateur" => 98, "adresse_utilisateur" => 21, "quantite" => 2 , "produits" => array( array("id_produits" => 5 , "quantite" => 2), array("id_produits" => 9 , "quantite" => 3)));
+					$panier = new Panier();					
+					$rep = $panier->enregistrer($array);
+					var_dump($rep);
+				}catch(Exception $e){
+					echo $e->getMessage();
+				}
 			?>
 		</div>
 		<div id="footer">
