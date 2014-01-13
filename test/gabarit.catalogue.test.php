9c7418cddf05f40e07b6a3aeda0f90a0153ba38e
Yan Boucher Bouchard <yan.bbouchard@gmail.com>
2014-01-13 08:45:50 -0500

    Commit for sync...
    Just to sync files...

--- test/gabarit.catalogue.test.php
+++ test/gabarit.catalogue.test.php
@@ -14,33 +14,49 @@
 			<?php 
 				// array des categories selon les cases selectionne
 				$aCategories = array(1=>true,2=>false,3=>false);
+				$catalogue 	 = new Catalogue($aCategories);
 			 ?>
 		</div>
 		<div id="contenu">
 			<h1>Test: afficher() tous les produits</h1>
 			<?php
-				$catalogue 	 = new Catalogue($aCategories);
-				$rep 		 = $catalogue->afficher("tous");
-				var_dump($rep);
+				try {
+					$rep = $catalogue->afficher("tous");
+					var_dump($rep);
+				} catch(Exception $e) {
+					echo $e->getMessage();
+				}	
 			?>
 			<h1>Test: afficher() par specs</h1>
 			<?php 
-				$catalogue = new Catalogue($aCategories);
-				$rep 	   = $catalogue->afficher("specs");
-				var_dump($rep);
+				try {
+					$rep = $catalogue->afficher("specs");
+					var_dump($rep);
+				} catch(Exception $e) {
+					echo $e->getMessage();
+				}
+					
 			?>
 			<h1>Test: afficher() par prix</h1>
 			<?php 
-				$catalogue = new Catalogue($aCategories);
-				$rep = $catalogue->afficher("prix");
-				var_dump($rep);
+				try {
+					$rep = $catalogue->afficher("prix");
+					var_dump($rep);
+				} catch(Exception $e) {
+					echo $e->getMessage();
+				}
 			?>
-			<h1>Test: enregistrer() non-fonctionnel</h1>
+			<h1>Test: enregistrer() fonctionnel</h1>
 			<?php 
-				$catalogue = new Catalogue($aCategories);
-				$array = array();
-				$rep = $catalogue->enregistrer($array);
-				var_dump($rep);
+				try {
+					$array = array("nomProduit"=>"Test","prixProduit"=>9.99,"descProduit"=>"Test description",);
+					$rep = $catalogue->enregistrer($array);
+					var_dump($rep);
+				} catch(Exception $e) {
+					echo $e->getMessage();
+				}
+				
+
 			?>
 		</div>
 		<div id="footer">
