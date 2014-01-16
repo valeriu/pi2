5cff2c1a40b34d99283f3f01028dfdb09a567c07
luc5mars <luc5mars@hotmail.com>
2014-01-16 12:49:58 -0500

    Config Taxes

--- includes/config.php
+++ includes/config.php
@@ -29,6 +29,23 @@
 	define("BD","e1295805");
 	define("PASS","50319801101");
 	
+	//TAXES PAR PROVINCES
+	define("CA", 0.05);
+	define("QC", 0.09975);
+	define("ON", 0.08);
+	define("MN", 0.08);
+	define("SK", 0.05);
+	define("CB", 0.07);
+	define("NB", 0.08);
+	define("NE", 0.1);
+	define("IE", 0.09);
+	define("AL", 1);
+	define("TN", 0.08);
+	define("NO", 1);
+	define("YK", 1);
+	define("NV", 1);
+	
+	
 // Google Maps API
 	define("GOOOGLEMAPAPI", "AIzaSyA65H6gOhP5ozo4DKYu0woD0tT7k2yKu78");
 ?>
